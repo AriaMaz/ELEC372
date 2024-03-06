4078b6737e3651b47c12e0fd7f19e61bd8fa30b5
ahmediqbaleng <146140095+ahmediqbaleng@users.noreply.github.com>
2024-03-06 18:54:37 -0500

    lol

--- ELEC372_Lab3/q9.m
+++ ELEC372_Lab3/q9.m
@@ -11,9 +11,9 @@ beta = 8/3;
 time_steps = 0:delta_t:t_max;
 %Lorenz system of equations
 
-lorenz_func = @(t, xyz)[xyz(2);
-                        xyz(3);
-                        xyz(3)-xyz(1)-1e-9*(exp(xyz(2)/0.026)-1)];
+lorenz_func = @(t, x, y, z)[y;
+                        z;
+                        z-x-1e-9*(exp(y/0.026)-1)];
 
 %ode solver, producing x,y,z values for the system
 
@@ -30,16 +30,16 @@ for i = 1:num_steps
     t = i+delta_t;
 
     %step 1
-    k1 = lorenz_func(t,[x, y, z]);
+    k1 = lorenz_func(t,x, y, z);
 
     %step 2
-    k2 = lorenz_func(t+(0.5*delta_t),[x+k1(1)*delta_t*0.5, y+k1(2)*delta_t*0.5, z+k1(3)*delta_t*0.5]);
+    k2 = lorenz_func(t+(0.5*delta_t),x+k1(1)*delta_t*0.5, y+k1(2)*delta_t*0.5, z+k1(3)*delta_t*0.5);
 
     %step 3
-    k3 = lorenz_func(t+(0.5*delta_t),[x+k2(1)*delta_t*0.5, y+k2(2)*delta_t*0.5, 3+k1(3)*delta_t]*0.5);
+    k3 = lorenz_func(t+(0.5*delta_t),x+k2(1)*delta_t*0.5, y+k2(2)*delta_t*0.5, 3+k1(3)*delta_t*0.5);
 
     %step 4
-    k4 = lorenz_func(t+delta_t,[x+k3(1)*delta_t, y+k3(2)*delta_t, 3+k3(3)*delta_t]);
+    k4 = lorenz_func(t+delta_t,x+k3(1)*delta_t, y+k3(2)*delta_t, 3+k3(3)*delta_t);
 
     x = x + (delta_t/6) * (k1(1) + 2*k2(1) + 2*k3(1) + k4(1));
     y = y + (delta_t/6) * (k1(2) + 2*k2(2) + 2*k3(2) + k4(2));
