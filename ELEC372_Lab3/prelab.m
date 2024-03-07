ccb7ccb1e009696a6728015ef25089dc96992625
AriaMaz <102880878+AriaMaz@users.noreply.github.com>
2024-03-06 19:54:40 -0500

    added x,y,z plots on seperate plots

--- ELEC372_Lab3/prelab.m
+++ ELEC372_Lab3/prelab.m
@@ -16,7 +16,7 @@ x = prelab_sol(:, 2);
 y = prelab_sol(:, 3);
 z = prelab_sol(:, 4);
 
-%question 1 plots
+%Question 1, all plots
 figure;
 plot(t, x, 'b');
 hold on;
@@ -25,12 +25,32 @@ plot(t, z, 'm');
 hold off;
 title('Lorenz system of equations solutions');
 xlabel('time(s)');
-ylabel('x(t)');
+ylabel('x(t), y(t), and z(t)');
 legend('x(t)', 'y(t)', 'z(t)');
 grid on;
 
+    %Question 1 x(t) plot
+    figure;
+    plot(t,y, 'r');
+    xlabel('time(s)');
+    ylabel('x(t)');
+    grid on;
+    
+    %Question 1, y(t) plot
+    figure;
+    plot(t,y, 'r');
+    xlabel('time(s)');
+    ylabel('y(t)');
+    grid on;
+    
+    %Question 1, z(t) plot
+    figure;
+    plot(t, z, 'm');
+    xlabel('time(s)');
+    ylabel('z(t)');
+    grid on;
 
-%Question 2 strange attractors y-x, z-x
+%Question 2, strange attractors y-x, z-x
 figure;
 plot(x, y);
 title('Strange Attractor of y(t) vs x(t)');
@@ -45,6 +65,7 @@ xlabel('x(t)');
 ylabel('z(t)');
 grid on;
 
+%Question 3, 3D trajectory
 figure;
 plot3(x, y, z);
 title('Three dimensional trajectory of [x(t), y(t), z(t)]');
@@ -52,6 +73,3 @@ xlabel('x(t)');
 ylabel('y(t)');
 zlabel('z(t)');
 grid on;
-
-
-%%hello World!
