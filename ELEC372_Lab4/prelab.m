3aac454e297b85f7a4dc2167adb9719d86963c32
AriaMaz <102880878+AriaMaz@users.noreply.github.com>
2024-03-20 18:34:14 -0400

    updated prelab

--- ELEC372_Lab4/prelab.m
+++ ELEC372_Lab4/prelab.m
@@ -4,18 +4,18 @@ f = @(x) 2*sin(x) - x.^2/30;
 % first derivative
 df = @(x) 2*cos(x) - x/15;
 
-% Plot function over range
+% plot function over range
 x = linspace(-6, 9, 1000);
 plot(x, f(x))
 hold on
 
-% Use initial guess based on plot and fzero to find zeros of first derivative 
+% yse initial guess based on plot and fzero to find zeros of first derivative 
 initialGuesses = [-5, 0, 2, 5, 8];
 zerosDf = zeros(1, length(initialGuesses));
 
 for i = 1:length(initialGuesses)
     zerosDf(i) = fzero(df, initialGuesses(i));
-    % Mark critical points on the graph
+    % mark critical points on the graph
     plot(zerosDf(i), f(zerosDf(i)), 'ro');
 end
 
@@ -25,6 +25,6 @@ xlabel('x')
 ylabel('f(x)')
 legend('f(x)', 'Critical Points', 'Location', 'best')
 
-% Output the critical points
+% output critical points
 disp('Critical points at x = ');
 disp(zerosDf);
